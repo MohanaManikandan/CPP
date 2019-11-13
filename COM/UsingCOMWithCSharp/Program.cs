1e67f78b39e847814fec05e1a8a98d7841c60ea1
MohanaManikandan <Manikandan@Win7VM>
2019-11-13 08:01:52 +0530

    Adds C# COM Interop To Source Control

---
+++ COM/UsingCOMWithCSharp/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Runtime.InteropServices;
+
+[InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+[Guid("4A476008-30A0-4CBA-9003-882EF4FCF68C")]
+interface IHen
+{
+    [PreserveSig]
+    void Cluck();
+
+    [PreserveSig]
+    void Roost();
+}
+
+[InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+[Guid("96F71D92-A188-4D29-8A92-C7FDF167C115")]
+interface IOnlineChicken
+{
+    // IHen
+    [PreserveSig]
+    void Cluck();
+
+    [PreserveSig]
+    void Roost();
+
+    // IOnlineChicken
+    [PreserveSig]
+    void Forage();
+}
+
+[InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+[Guid("630FDA7E-15E4-43B4-81E4-B0EB2B70D1C7")]
+interface IOfflineChicken
+{
+    [PreserveSig]
+    void Load();
+
+    [PreserveSig]
+    void Save();
+}
+
+namespace UsingCOMWithCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Using IHen Methods
+            IHen pHen = CreateHen();
+            pHen.Cluck();
+            pHen.Roost();
+
+            // QueryInterface Is Achieved Through Something Like A C-Style Cast
+            IOnlineChicken pOnlineChicken = (IOnlineChicken)pHen;
+            pOnlineChicken.Forage();
+        }
+
+        [DllImport("ImplementingIUnkown.dll", PreserveSig = false)]
+        static extern IHen CreateHen();
+    }
+}
